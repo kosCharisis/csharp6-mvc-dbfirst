a753c16890950a75f968a6a64e668ad254ee87bf
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-11-30 01:52:06 +0200

    updates

---
+++ Repositories/ITeacherRepository.cs
@@ -0,0 +1,17 @@
+﻿using SchoolApp.Data;
+using SchoolApp.Models;
+
+namespace SchoolApp.Repositories
+{
+    public interface ITeacherRepository : IBaseRepository<Teacher>
+    {
+        Task<List<Course>> GetTeacherCoursesAsync(int id);
+        Task<Teacher?> GetByPhoneNumberAsync(string phoneNumber);
+        Task<List<User>> GetAllUsersTeacherAsync();
+        Task<List<User>> GetAllUsersTeachersPaginatedAsync(int pageNumber, int pageSize);
+        Task<User?> GetUserTeacherByUsernameAsync(string username);
+        Task<PaginatedResult<User>> GetPaginatedUsersTeachersAsync(int pageNumber, int pageSize);
+        Task<PaginatedResult<User>> GetPaginatedUsersTeachersFilteredAsync(int pageNumber, int pageSize,
+            List<Func<User, bool>> predicates);
+    }
+}
