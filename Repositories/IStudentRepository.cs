2fcdad63aae411bc7b8b616073a04583722783d0
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-11-30 14:29:26 +0200

    updates

---
+++ Repositories/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using SchoolApp.Data;
+
+namespace SchoolApp.Repositories
+{
+    public interface IStudentRepository
+    {
+        Task<List<Course>> GetStudentCourseAsync(int id);
+        Task<Student?> GetByAm(string? am);
+        Task<List<User>> GetAllUsersStudentsAsync();
+    }
+}
---
+++ Repositories/StudentRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolApp.Core.enums;
+using SchoolApp.Data;
+
+namespace SchoolApp.Repositories
+{
+    public class StudentRepository : BaseRepository<StudentRepository>, IStudentRepository
+    {
+        public StudentRepository(Mvc6DbContext context) 
+            : base(context)
+        {
+        }
+
+        public async Task<Student?> GetByAm(string? am)
+        {
+            return await context.Students
+                .Where(s => s.Am == am)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<List<Course>> GetStudentCourseAsync(int id)
+        {
+            return await context.Students
+                .Where(s => s.Id == id)
+                .SelectMany(s => s.Courses)
+                .ToListAsync();
+        }
+
+        public async Task<List<User>> GetAllUsersStudentsAsync()
+        {
+            return await context.Users
+                .Where(u => u.UserRole == UserRole.Student)
+                .Include(u => u.Student)
+                .ToListAsync();
+        }
+    }
+}
