305ce301245eb2382a4415e8cfcdb971f4cdd6f9
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-11-30 17:17:58 +0200

    Exceptions

---
+++ Services/ITeacherService.cs
@@ -0,0 +1,15 @@
+﻿using SchoolApp.Data;
+using SchoolApp.DTO;
+
+namespace SchoolApp.Services
+{
+    public interface ITeacherService
+    {
+        Task SignUpUserAsync(TeacherSignuUpDTO request);
+        Task<List<User>> GetAllUsersTeachersAsync();
+        Task<List<User>> GetAllUsersTeachersAsync(int pageNumber, int pageSize);
+        Task<int> GetTeacherCountAsync();
+        Task<User?> GetTeacherByUsernameAsync(string username);
+
+    }
+}
